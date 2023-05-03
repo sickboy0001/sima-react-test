0f7251dc90d615959574e85c36bde9e24e31c2e5
tfujiki <syunjyu0001@gmail.com>
2023-05-03 16:22:19 +0900

    UseStat関数含めることで、再利用できる？

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,39 +3,26 @@ import styles from '../styles/Home.module.css'
 import { Footer } from "../components/Footer"
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
-import { useCallback, useEffect } from 'react'
-
-
-
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
-  const foo = 1
-  //再描画の回避＝UseCallBack
-  const handleClick=useCallback((e:any)=>  {
-    console.log(e.target)
-    e.preventDefault();
-    // aleart(foo);
-    alert(foo)
-  },[]);
-  //マウント、アンマウント　ライフサイクル
 
+  const [count,setCount]  =useState(1)
+  const handleClick = (e:any)=>  {
+    setCount((count)=>count*2)
+    setCount((count)=>count*2)
+  };
 
   return (
     <div className={styles.container}>
-      {/* <div 
-      onClick={(e)=>{
-        handleClick(e)
-      }}>
-      test onclick
-      </div> */}
       <Head>
         <title>Index</title>
       </Head>
       <Header/>
-      <a 
-        href='/about'
+      <h1>{count}</h1>
+      <button
         onClick={handleClick}
-      >button</a>
+      >button</button>
       <Main page="index"/>
       <Footer/>
     </div>
