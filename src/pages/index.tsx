98687dbb2c2406e8f5f853dded75967d2ccbd663
tfujiki <syunjyu0001@gmail.com>
2023-05-03 10:04:10 +0900

    CallBack関数のテスト

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,37 @@ import styles from '../styles/Home.module.css'
 import { Footer } from "../components/Footer"
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
+import { useCallback } from 'react'
+
+
 
 
 export default function Home() {
+  const foo = 1
+
+  //再描画の回避＝UseCallBack
+  const handleClick=useCallback((e:any)=>  {
+    console.log(e.target)
+    e.preventDefault();
+    // aleart(foo);
+    alert(foo)
+  },[]);
+
   return (
     <div className={styles.container}>
+      <div onClick={(e)=>{
+        handleClick(e,foo)
+      }}>
+      test onclick
+      </div>
       <Head>
         <title>Index</title>
       </Head>
       <Header/>
+      <a 
+        href='/about'
+        onClick={handleClick}
+      >button</a>
       <Main page="index"/>
       <Footer/>
     </div>
