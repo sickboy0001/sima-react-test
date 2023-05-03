6bb6d49bf1f184d0ec8e88bf2005495cee5714b0
tfujiki <syunjyu0001@gmail.com>
2023-05-04 07:08:01 +0900

    イミュータブル、スプレッド構文

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,30 @@ export default function Home() {
   const [count,setCount]  =useState(1)//状態？
   const [text,setText] = useState("")
   const [isShow,setIsShow] = useState(true)
+  const [array,setArray] = useState([])
   const handleDisplay = () => {
-    setIsShow(isShow=> ! isShow)
+    setIsShow(prevIsShow=> ! prevIsShow)
   };
+  const handleAdd = useCallback(
+    ()=>{
+        setArray(
+          (prevArray:any)=>
+            {
+              if(prevArray.some((item: string) => item === text))
+              {
+                alert("同じ要素があります。")
+                return prevArray; 
+              }
+              const newArray = [...prevArray,text]
+              return newArray;
+          }
+          )
+    },[text])
+
   const handleClick = useCallback(
     (e:any)=>  {
       if(count<100){
-        setCount((foo)=>foo*2)
+        setCount((prevCount)=>prevCount*2)
       }
     },[count]//意識する必要ない。レスポンスのために最適化する設定ぽい？
   );
@@ -53,6 +70,18 @@ export default function Home() {
       <input type="text" value={text} 
         onChange={handelChange}
         />
+        <div>
+          <button onClick={handleAdd}>
+            handleaAdd
+          </button>
+        </div>
+        <ul>
+          {array.map(item => {
+            return(
+              <li key={item}>{item}</li>
+            )
+          })}
+        </ul>
       <Main page="index"/>
       <Footer/>
     </div>
